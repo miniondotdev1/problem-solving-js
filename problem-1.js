bbbf664ad845c8ad279113cd8bb84d9af0ca9cfe
my first github repo <mrarifmondol98@gmail.com>
2026-07-26 12:06:30 +0600

    Problem solving for even or odd number

---
+++ problem-1.js
@@ -0,0 +1,11 @@
+// problem-1: Check is a number is Even or Odd
+
+function checkEvenOrOdd(number){
+    if(number % 2 === 0){
+        return "Even Number"
+    }else{
+        return "Odd Number"
+    }
+}
+
+console.log(checkEvenOrOdd(501))
