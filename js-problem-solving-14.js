ca3ffb44d6539d7256a2679611b80687c8557832
my first github repo <mrarifmondol98@gmail.com>
2026-07-31 10:32:21 +0600

    JS problem solving

---
+++ js-problem-solving-14.js
@@ -0,0 +1,16 @@
+// Input: an object
+// Output: array of the object's keys
+// Returns: an array
+ 
+function listKeys(obj) {
+  let keys = [];
+  
+  for(let key in obj){
+    keys.push(key)
+  }
+ 
+  return keys;
+}
+ 
+console.log(listKeys({ name: "Sam", age: 25, city: "Dhaka" }));
+// Expected: ["name", "age", "city"]
