f65d50756b49f19781421d312bcbfeba9bdfb36d
my first github repo <mrarifmondol98@gmail.com>
2026-08-03 22:53:36 +0600

    JS problem solving

--- problem-13.js
+++ problem-13.js
@@ -10,7 +10,21 @@ let monitor = {
 
 // First way
 for(let key in monitor){
-    console.log(key, ": ", monitor[key]);
+    // console.log(key, ": ", monitor[key]);
 }
 
 // keys method
+
+let objKey = Object.keys(monitor)
+
+
+for (let i = 0; i < objKey.length; i++) {
+  let key = objKey[i]
+//   console.log(key, ': ', monitor[key]);
+}
+
+// another method 
+
+for( let key of objKey){
+    console.log(key, ': ' , monitor[key]);
+}
