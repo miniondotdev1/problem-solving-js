af17b16c88131f7ace98379bbc471cc7ad47539c
my first github repo <mrarifmondol98@gmail.com>
2026-08-03 23:33:05 +0600

    JS problem solving

--- problem-14.js
+++ problem-14.js
@@ -1,1 +1,39 @@
-// 
+// Problem 14: you are given object containing information about a student.
+
+// Tasks
+/**
+ * print all keys. 
+ * print all values.
+ * print each key-value pair.
+ * // name: Uthso
+ * // age: 26
+ * //university: DIU
+ * // department: CSE
+ * 
+ * Count the total number of properties in the object. 
+ * check if the object contains a property named "email"
+ * 
+ * // expected input:
+ * const student  {
+ * // name: Uthso
+ * // age: 26
+ * //university: DIU
+ * // department: CSE
+ * };
+ * 
+ * // expected outpur:
+ * 
+ * name age university department
+ * 
+ * Uthso 26 DIU CSE
+ * 
+ * name: uthso
+ * age: 26
+ * university; DIU
+ * department: CSE
+ * 
+ * Total properties: 4
+ * Has Email: false
+ * 
+ * 
+*/
