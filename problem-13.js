f1dd6346bf38ac6a0a275c93ee0d993e216518bc
my first github repo <mrarifmondol98@gmail.com>
2026-08-03 22:48:46 +0600

    js problem solving

--- problem-13.js
+++ problem-13.js
@@ -1,2 +1,16 @@
 // Problem 13: Loop through an object's properties
 
+let monitor = {
+    color: "black",
+    brand: "LG",
+    display: "Something",
+    size: "Something",
+    test: "Dummy"
+}
+
+// First way
+for(let key in monitor){
+    console.log(key, ": ", monitor[key]);
+}
+
+// keys method
