bd2b925d1c37482125e60240100a4b8f106b0cad
my first github repo <mrarifmondol98@gmail.com>
2026-08-04 20:22:26 +0600

    JS problem solving

--- problem-14.js
+++ problem-14.js
@@ -37,3 +37,33 @@
  * 
  * 
 */
+
+const student = {
+    name: "Utsho",
+    age: 25,
+    university: "DIU",
+    department: "CSE"
+};
+
+function objOperation(obj){
+    for(let key in obj){
+        console.log(key)
+    }
+
+    for (let key in obj) {
+      console.log(obj[key]);
+    }
+    for (let key in obj) {
+      console.log(key, ": ", obj[key]);
+    }
+
+    let objKeyCount = Object.keys(obj).length
+    console.log("Total properties: ", objKeyCount);
+
+    let hasEmailProperty = obj.hasOwnProperty("email")
+    console.log("Has Email: ", hasEmailProperty)
+
+}
+
+objOperation(student)
+
